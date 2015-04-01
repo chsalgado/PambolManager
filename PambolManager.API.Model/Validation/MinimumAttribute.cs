1ca71ed778c1ca66a83ea58d1715717e82da81e6
chrisave4 <chrisave4@hotmail.com>
2015-03-31 20:17:42 -0600

    Created Minimum and Maximum validation attributes.

---
+++ PambolManager.API.Model/Validation/MinimumAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace PambolManager.API.Model.Validation
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MinimumAttribute : ValidationAttribute
+    {
+        private readonly int _minimumValue;
+
+        public MinimumAttribute(int minimum) :
+            base(errorMessage: "The {0} field value must be minimum {1}.")
+        {
+
+            _minimumValue = minimum;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+
+            return string.Format(
+                CultureInfo.CurrentCulture,
+                base.ErrorMessageString,
+                name,
+                _minimumValue);
+        }
+
+        public override bool IsValid(object value)
+        {
+
+            int intValue;
+            if (value != null && int.TryParse(value.ToString(), out intValue))
+            {
+
+                return (intValue >= _minimumValue);
+            }
+
+            return false;
+        }
+
+    }
+}
